6444a2a844912dec42186b41fa2ca78f9bad8b9e
nfhsathlete2 <65311500+nfhsathlete2@users.noreply.github.com>
2021-08-12 19:10:33 -0400

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+// Get Bill Input
+const bill = parseFloat(document.querySelector('#inputs-bill').value);
+
+// Get Tip Input
+const tip = document.querySelector('#inputs-bill');
+
+// Get People Input
+const people = parseFloat(document.querySelector('#inputs-people').value);
+
+// Multiply Bill by the Tip Amount = tipResult
+// Divide tipResult by People
+// Divide Bill by People = perPerson
+// Add tipResult to PerPerson = totalPerPerson
+
+
+// NOTES
+// update inputs when changed
+
+// Change color of div when clicked
+
+
